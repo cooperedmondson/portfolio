9efc32b435904e513eb9bc2987fcb3575b6c971f
Cooper Edmondson <cooperedmondson0115@gmail.com>
2022-12-17 18:14:17 -0500

    HOLY SHIIIIIT

--- ghi/src/components/Main.js
+++ ghi/src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import fawef from '../assets/fawef.png'
-import GOOGLY from '../assets/GOOGLY.png'
+import PAPER from '../assets/PAPER.gif'
 import BAGEL from '../assets/BAGEL.png'
 import JOBI from '../assets/JOBI.gif'
 
@@ -8,38 +8,14 @@ import { useAnimation, motion } from 'framer-motion';
 
 
 export default function Main() {
-    const [scrollY, setScrollY] = useState(0);
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    function handleScroll() {
-        setScrollY(window.scrollY);
-    }
-
-    const threshold = 100; // the number of pixels the user must scroll down before the div starts moving
-    const amount = scrollY - threshold; // the amount by which the div should be translated
-    const translateY = amount > 0 ? -amount / 2 : 0; // the translateY value to use in the transform propertythe translateY value to use in the transform propertys
-    ////////////////////////////////
-
-
-
-
-
+    console.log(window.pageYOffset)
 
     const useScrollPosition = () => {
         const [testScroll, setTestScroll] = useState(window.pageYOffset);
 
         const onScroll = () => {
-            const threshold = 880;  // set the threshold for the limiter
-            if (window.pageYOffset > threshold) {  // only start the animation if the scroll position is greater than the threshold
-                setTestScroll(window.pageYOffset);
-            }
-        };
+            setTestScroll(window.pageYOffset);
+        }
 
         useEffect(() => {
             window.addEventListener('scroll', onScroll);
@@ -50,6 +26,40 @@ export default function Main() {
         return testScroll;
     };
 
+    const useScrollTest = () => {
+        const [scrollTest, setScrollTest] = useState(window.pageYOffset);
+
+        const onScrolling = () => {
+            setScrollTest(window.pageYOffset);
+        };
+
+        useEffect(() => {
+            window.addEventListener('scroll', onScrolling);
+            return () => {
+                window.removeEventListener('scroll', onScrolling);
+            };
+        }, []);
+
+        const startThreshold = 1400; 
+        const stopThreshold = 2300;
+        const scrollPosition = window.scrollY;
+        let divTop;
+        if (scrollPosition < startThreshold) {
+            divTop = startThreshold;
+        } else if (scrollPosition > stopThreshold) {
+            divTop = stopThreshold;
+        } else {
+            divTop = scrollPosition;
+        }
+
+        const divStyle = {
+            transform: `translate3d(0%, calc(${divTop * 0.65 - 1200}px - 0%), 0)`,
+        };
+
+        return { scrollTest, divStyle };
+    };
+
+    const { scrollTest, divStyle } = useScrollTest();
     const position = useScrollPosition();
 
     return (
@@ -68,49 +78,49 @@ export default function Main() {
                 <div className='rotate'>
                     <img src={fawef} className='image' />
                 </div>
-                <div className='cont px-10'>
-                    <div className='bg-transparent text-white' id='introduction'>
-                        <div>
-                            <p className='bg-transparent'>
-                                HELLO! I am a 21 year old
-                                <span className='span-p'>Software Engineer</span>
-                                . Currently looking for my first role!
-                            </p>
-                        </div>
-                    </div>
-                    <div className=""
-                        style={{
-                            position: 'relative',
-                            width: '20%',
-                            height: '100%',
-                            animationDuration: '2s',
-                            // backgroundColor: '#6A6A6A',
-                            backgroundImage: `url("https://media4.giphy.com/media/rcFs59ww1R7yeFszmo/giphy.gif")`,
-                            transition: 'transform 4s ease-out',
-                            // margin: '2rem',
-                            transform: `translate3d(0, ${-translateY / 14}px, 0)`,
-                            overflow: 'hidden',
-                        }}
-                    >
-                        <div style={{ position: 'relative' }} className="bg-transparent">
-                            <img
-                                src={BAGEL}
-                                style={{
-                                    position: 'absolute',
-                                    top: '100px',
-                                    zIndex: 1,
-                                    transform: `translate3d(0, ${translateY / 5}px, 0)`,
-                                }}
-                                className="bg-transparent"
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div id='about'>
-                    <br />
-                </div>
+                <div className='py-40'>
 
-                <div className=' flex gap-10 px-20 mt-40'>
+                </div>
+                <div className='jobu-wrap'>
+                    <div className='text-parent z-1 bg-transparent'>
+                        <h1 className='h12'
+                            style={{
+                                transform: `translate3d(-${position / 2 - 40}px, -${position * 0.1}px, 0px)`
+                            }}
+                        >
+                            A FULLSTACK DEVELOPER
+                        </h1>
+                        <h1 className='h12'
+                            style={{
+                                transform: `translate3d(${position / 2 - 400}px, -${position * 0.1}px, 0px)`
+                            }}
+                        >
+                            WITH A CREATIVE MIND
+                        </h1>
+                    </div>
+                    <img className="jobu" src={JOBI}
+                        style={{
+                            transform: `translate3d(-50%, calc(-${position * 0.26}px - 0%), 0)`,
+                        }}
+                    />
+                    <div className='text-parent z-10 bg-transparent'>
+                        <h1 className='h12 out'
+                            style={{
+                                transform: `translate3d(-${position / 2 - 40}px, -${position * 0.1}px, 0px)`
+                            }}
+                        >
+                            A FULLSTACK DEVELOPER
+                        </h1>
+                        <h1 className='h12 out '
+                            style={{
+                                transform: `translate3d(${position / 2 - 400}px, -${position * 0.1}px, 0px)`
+                            }}
+                        >
+                            WITH A CREATIVE MIND
+                        </h1>
+                    </div>
+                </div>
+                <div className='flex gap-10 ' id='intro'>
                     <div className='bg-transparent text-white'>
                         <h1 className='about'> About Me </h1>
                     </div>
@@ -144,57 +154,15 @@ export default function Main() {
                         </div>
                     </div>
                 </div>
-                <div className='py-60'>
+
+
+
+
+                <div className='moving-container'
+                    style={divStyle}
+                >
+                    <img src={PAPER} className="papa" />
                 </div>
-
-
-
-
-
-
-                <div className='jobu-wrap'>
-                    <div className='text-parent z-1 bg-transparent'>
-                        <h1 className='h12'
-                            style={{
-                                transform: position >= threshold ? `translate3d(-${position / 4 - 150}px, -${position * 0.1}px, 0px)` : `none`
-                            }}
-                        >
-                            A FULLSTACK DEVELOPER
-                        </h1>
-                        <h1 className='h12'
-                            style={{
-                                transform: position >= threshold ? `translate3d(${position / 4 - 360}px, -${position * 0.1}px, 0px)` : `none`
-                            }}
-                        >
-                            WITH A CREATIVE MIND
-                        </h1>
-                    </div>
-                    <img className="jobu" src={JOBI}
-                        style={{
-                            transform: `translate3d(-50%, calc(-${position * 0.26}px - -30%), 0)`,
-                        }}
-                    />
-                    <div className='text-parent z-10 bg-transparent'>
-                        <h1 className='h12 out'
-                            style={{
-                                transform: position >= threshold ? `translate3d(-${position / 4 - 150}px, -${position * 0.1}px, 0px)` : `none`
-                            }}
-                        >
-                            A FULLSTACK DEVELOPER
-                        </h1>
-                        <h1 className='h12 out '
-                            style={{
-                                transform: position >= threshold ? `translate3d(${position / 4 - 360}px, -${position * 0.1}px, 0px)` : `none`
-                            }}
-                        >
-                            WITH A CREATIVE MIND
-                        </h1>
-                    </div>
-                </div>
-
-
-
-
 
 
             </div>
--- ghi/src/components/asdasd.js
+++ ghi/src/components/asdasd.js
@@ -1,204 +1,34 @@
-import React, { useState, useRef, useEffect } from 'react';
-import fawef from '../assets/fawef.png'
-import GOOGLY from '../assets/GOOGLY.png'
-import BAGEL from '../assets/BAGEL.png'
-import { useAnimation, motion } from 'framer-motion';
+    const useScrollTest = () => {
+        const [scrollTest, setScrollTest] = useState(window.pageYOffset);
 
-
-
-export default function Main() {
-    const [scrollY, setScrollY] = useState(0);
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
+        const onScrolling = () => {
+            setScrollTest(window.pageYOffset);
         };
-    }, []);
 
-    function handleScroll() {
-        setScrollY(window.scrollY);
-    }
+        useEffect(() => {
+            window.addEventListener('scroll', onScrolling);
+            return () => {
+                window.removeEventListener('scroll', onScrolling);
+            };
+        }, []);
 
-    const threshold = 100; // the number of pixels the user must scroll down before the div starts moving
-    const amount = scrollY - threshold; // the amount by which the div should be translated
-    const translateY = amount > 0 ? -amount / 2 : 0; // the translateY value to use in the transform propertythe translateY value to use in the transform propertys
-    ////////////////////////////////
+        const startThreshold = 1400; // distance from top of page where div should start moving
+        const stopThreshold = 2000; // distance from top of page where div should stop moving
+        const scrollPosition = window.scrollY;
+        const viewportHeight = window.innerHeight;
 
+        let divTop;
+        if (scrollPosition < startThreshold) {
+            divTop = startThreshold;
+        } else if (scrollPosition > stopThreshold) {
+            divTop = stopThreshold;
+        } else {
+            divTop = scrollPosition;
+        }
 
-    const [topX, setTopX] = useState(0);
-    const [bottomX, setBottomX] = useState(0);
-    const [prevScrollY, setPrevScrollY] = useState(0);
-    const [direction, setDirection] = useState(null);
+        const divStyle = {
+            transform: `translate3d(0%, calc(${divTop * 0.03}px - 0%), 0)`,
+        };
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            // Update the direction of the user's scroll
-            if (prevScrollY > window.scrollY) {
-                setDirection("up");
-            } else if (prevScrollY < window.scrollY) {
-                setDirection("down");
-            }
-
-            // Update the position of the elements as the user scrolls
-            if (window.scrollY === 0) {
-                setTopX(0);
-                setBottomX(0);
-            } else if (
-                direction === "down" &&
-                window.scrollY >= 1000 &&
-                window.scrollY < 3000 &&
-                topX > -document.body.clientWidth
-              ) {
-                setTopX(topX - 5);
-              }
-              if (
-                direction === "down" &&
-                window.scrollY >= 1000 &&
-                window.scrollY < 3000 &&
-                bottomX < document.body.clientWidth
-              ) {
-                setBottomX(bottomX + 5);
-              }
-              if (
-                direction === "up" &&
-                window.scrollY >= 1000 &&
-                window.scrollY < 3000 &&
-                topX < document.body.clientWidth
-              ) {
-                setTopX(topX + 5);
-              }
-              if (
-                direction === "up" &&
-                window.scrollY >= 1000 &&
-                window.scrollY < 3000 &&
-                bottomX > -document.body.clientWidth
-              ) {
-                setBottomX(bottomX - 5);
-              }
-            setPrevScrollY(window.scrollY);
-        });
-    }, [topX, bottomX, prevScrollY, direction]);
-
-
-
-    return (
-        <div className='grain-effect'>
-            <div className='main-1' >
-                <nav className=' bg-transparent text-white uppercase font-semibold' id='col-3'>
-                    <div className='bg-transparent'>
-                        <a href='#about'>About</a>
-                        <br />
-                        <a>Projects</a>
-                    </div>
-                    <div className='bg-transparent'>
-                        <a>Github</a>
-                    </div>
-                </nav>
-                <div className='rotate'>
-                    <img src={fawef} className='image' />
-                </div>
-                <div className='cont px-10'>
-                    <div className='bg-transparent text-white' id='introduction'>
-                        <div>
-                            <p className='bg-transparent'>
-                                HELLO! I am a 21 year old
-                                <span className='span-p'>Software Engineer</span>
-                                . Currently looking for my first role!
-                            </p>
-                        </div>
-                    </div>
-                    <div className=""
-                        style={{
-                            position: 'relative',
-                            width: '20%',
-                            height: '100%',
-                            animationDuration: '2s',
-                            // backgroundColor: '#6A6A6A',
-                            backgroundImage: `url("https://media4.giphy.com/media/rcFs59ww1R7yeFszmo/giphy.gif")`,
-                            transition: 'transform 4s ease-out',
-                            // margin: '2rem',
-                            transform: `translate3d(0, ${-translateY / 14}px, 0)`,
-                            overflow: 'hidden',
-                        }}
-                    >
-                        <div style={{ position: 'relative' }} className="bg-transparent">
-                            <img
-                                src={BAGEL}
-                                style={{
-                                    position: 'absolute',
-                                    top: '100px',
-                                    zIndex: 1,
-                                    transform: `translate3d(0, ${translateY / 5}px, 0)`,
-                                }}
-                                className="bg-transparent"
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div id='about'>
-                    <br />
-                </div>
-
-                <div className=' flex gap-10 px-20 mt-40'>
-                    <div className='bg-transparent text-white'>
-                        <h1 className='about'> About Me </h1>
-                    </div>
-                    <div className='text-white' id='hello'>
-                        <div className='text-container'>
-                            <div className='uppercase' id="p-text">
-                                <div className='b1'>
-                                    <div>FullStack</div>
-                                    <div>Developer</div>
-                                </div>
-                                <div className='b1'>
-                                    <div>BackEnd</div>
-                                    <div>Developer</div>
-                                </div>
-                                <div className='b1'>
-                                    <div>FrontEnd</div>
-                                    <div>Developer</div>
-                                </div>
-                            </div>
-                            <br />
-                            <div className='boh'>
-                                <p className='b2'>
-                                    I'm a recent graduate of Hack Reactor's
-                                    bootcamp. I've also graduated from Abraham
-                                    Baldwin Agricultural College with my Associates
-                                    of Science. I love the outdoors, movies, and learning
-                                    about new technologies. Feel free to connect with me and
-                                    chat!
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='py-40'>
-
-                </div>
-
-                <div className="text-white">
-                    <motion.div
-                        style={{
-                            position: "absolute",
-                            transform: `translate3d(${topX}px, -90px, 0)`
-                        }}
-                    >
-                        <h1 className="hug">A FULL STACK DEVELOPER</h1>
-                    </motion.div>
-                    <motion.div
-                        style={{
-                            position: "absolute",
-                            right: 0,
-                            transform: `translate3d(${bottomX}px, 90px, 0)`
-                        }}
-                    >
-                        <h1 className="hug bg-transparent">WITH A CREATIVE MIND</h1>
-                    </motion.div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
+        return { scrollTest, divStyle };
+    };
